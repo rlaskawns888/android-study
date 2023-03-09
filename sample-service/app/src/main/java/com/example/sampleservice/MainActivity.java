a91802eaf219f55266db00750c7aca8ef3344d63
knj <rlaskawns888@gmail.com>
2023-03-09 12:39:05 +0900

    service to MainAcitivity Intent send

--- sample-service/app/src/main/java/com/example/sampleservice/MainActivity.java
+++ sample-service/app/src/main/java/com/example/sampleservice/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,6 +37,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //액티비티가 새로 만들어질 때 전달될 인텐트 처리
+        Intent passedIntent = getIntent();
+        passedIntent(passedIntent);
+    }
 
+    //액티비티 메모라가 생성되어있을 경우 호출됨.
+    @Override
+    protected void onNewIntent(Intent intent) {
+        passedIntent(intent);
+        super.onNewIntent(intent);
+        //액티비티가 이미 만들어져 있을 때 전달된 인텐트 처리
+    }
+
+    private void passedIntent(Intent intent) {
+        if(null != intent) {
+            String command = intent.getStringExtra("command");
+            String name = intent.getStringExtra("name");
+
+            Toast.makeText(this, "command: " + command + ", name: " + name, Toast.LENGTH_LONG).show();
+        }
     }
 }
--- sample-service/app/src/main/java/com/example/sampleservice/MyService.java
+++ sample-service/app/src/main/java/com/example/sampleservice/MyService.java
@@ -36,14 +36,33 @@ public class MyService extends Service {
         Log.d(TAG, "processCommand() -> command: " + command);
         Log.d(TAG, "processCommand() -> name: " + name);
 
-        for(int i=0; i<5; i++) {
-            try {
-                Thread.sleep(1000);
-                Log.d(TAG, "Waiting" + i + " s.");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+        //ex1)
+//        for(int i=0; i<5; i++) {
+//            try {
+//                Thread.sleep(1000);
+//                Log.d(TAG, "Waiting" + i + " s.");
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+
+        //ex2)
+        //MainAcitivty에서 Service로 전달받은 데이터를
+        //다시 Service에서 MainAcivity로 전달.
+        Intent showIntent = new Intent(getApplicationContext(), MainActivity.class);
+        showIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                            Intent.FLAG_ACTIVITY_SINGLE_TOP |
+                            Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        //FLAG_ACTIVITY_NEW_TASK: 새로운 테스트그를 생성하도록 플래그 생성
+        // (서비스는 화면이 없기 때문에 화면이 없는 서비스에서 화면이 있는 액티비를
+        // 띄우려면 새로운 태스크 생성이 필요함)
+
+        //FLAG_ACTIVITY_SINGLE_TOP, FLAG_ACTIVITY_CLEAR_TOP
+        //: MainAcitivty가 이미 메모리가 있을 때 재사용하도록 위의 플래그 2개를 추가함.
+
+        showIntent.putExtra("command", "show");
+        showIntent.putExtra("name", name + " from service.");
+        startActivity(showIntent);
     }
 
     @Override
