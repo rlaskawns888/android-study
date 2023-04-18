060b49586472c2f4d51852ba96f3cf35d359f0c2
knj <rlaskawns888@gmail.com>
2023-04-18 12:50:59 +0900

    Google Gson

---
+++ sample-request2/app/src/main/java/com/example/samplerequest2/MovieList.java
@@ -0,0 +1,5 @@
+package com.example.samplerequest2;
+
+public class MovieList {
+    MovieListResult boxOfficeResult;
+}
---
+++ sample-request2/app/src/main/java/com/example/samplerequest2/MovieListResult.java
@@ -0,0 +1,10 @@
+package com.example.samplerequest2;
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyboxOfficeList = new ArrayList<Movie>();
+}
